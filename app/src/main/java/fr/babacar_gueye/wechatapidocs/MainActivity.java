a585b68562c6dbc59ec7ef64b439e5acb5d58261
Babacar GUEYE <gueyens11@gmail.com>
2015-03-23 00:14:11 +0800

    added Webview - Enabled javascript - Enabled back button navigation

--- app/src/main/java/fr/babacar_gueye/wechatapidocs/MainActivity.java
+++ app/src/main/java/fr/babacar_gueye/wechatapidocs/MainActivity.java
@@ -4,16 +4,34 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
 public class MainActivity extends Activity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
