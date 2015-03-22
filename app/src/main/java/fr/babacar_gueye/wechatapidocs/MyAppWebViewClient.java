5cc9225d6498d397d162b846b14bad9c2374663e
Babacar Gueye <babacar.gueye@epitech.eu>
2015-03-23 00:52:18 +0800

    add override WebViewClient for navigation

---
+++ app/src/main/java/fr/babacar_gueye/wechatapidocs/MyAppWebViewClient.java
@@ -0,0 +1,22 @@
+package fr.babacar_gueye.wechatapidocs;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Babacar on 23/03/2015.
+ */
+public class MyAppWebViewClient extends WebViewClient{
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (Uri.parse(url).getHost().length() == 0) {
+            return false;
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+}
